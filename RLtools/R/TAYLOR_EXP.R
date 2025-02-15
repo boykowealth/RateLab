bf63d6d187756a339f57b191085fe743887fa02b
Brayden Boyko <braydenboyko@boykowealth.com>
2025-02-15 12:34:23 -0700

    Fixed Error Messages Related To RLtools Package

--- RLtools/R/TAYLOR_EXP.R
+++
@@ -1,3 +0,0 @@
-
-
-TAYLOR_EXP <- 
--- global.R
+++ global.R
@@ -1,8 +1,8 @@
-devtools::install_github()
-
+devtools::install_github('boykowealth/RateLab', subdir = "RLtools")
 
 library(dplyr)
 library(tidyverse)
 library(tidyquant)
+library()
 
 
