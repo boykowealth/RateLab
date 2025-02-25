c47de3c4cd97a42911ddbb58e4e434c8175b20b6
Brayden Boyko <braydenboyko@boykowealth.com>
2025-02-24 17:23:12 -0700

    Added Environment

--- ui.R
+++ ui.R
@@ -5,21 +5,28 @@ ui <- bslib::page_navbar(
   theme = bslib::bs_theme(bootswatch = "yeti"),
   
   bslib::nav_panel(
+    title = "Enviorment",
+    bslib::layout_columns(
+      bslib::card(
+        bslib::card_header("Filters")
+      )
+    )
+  ),
+  
+  bslib::nav_panel(
     title = "Portfolio",
     bslib::layout_columns(
       col_widths = c(2, 10),
       bslib::card(
-        bslib::card_header("Manage Your Book"),
+        bslib::card_header("Manage Positions"),
         shiny::uiOutput("dynamicUI"),
         shiny::actionButton("add", "Add Position")
       ),
       
       bslib::card(
-        bslib::card_header("Your Portfolio")
+        bslib::card_header("Portfolio View")
       )
-      
     )
-    
   ),
   
   bslib::nav_panel(
@@ -40,15 +47,6 @@ ui <- bslib::page_navbar(
       )
     )
   ), 
-  
-  bslib::nav_panel(
-    title = "Trading",
-    bslib::layout_columns(
-      bslib::card(
-        bslib::card_header("Filters")
-      )
-    )
-  ),
   
   bslib::nav_panel(
     title = "Documentation",
